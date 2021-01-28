bed82e54761117bd7580ff30f236dd8ae1c4a4fa
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-01-28 19:06:29 +0000

    Missing commit

--- Crypto/SceneDelegate.swift
+++ Crypto/SceneDelegate.swift
@@ -12,4 +12,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScen
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and at
