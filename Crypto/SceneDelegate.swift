6fc120dc7b24db386f207a2f7f5f1998d6ad4423
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-04-02 11:13:10 +0000

    Remove log message

--- Crypto/SceneDelegate.swift
+++ Crypto/SceneDelegate.swift
@@ -37,4 +37,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene tr
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the cha
