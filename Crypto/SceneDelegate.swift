2ef0d8432a596e98fc4a3cefdc48bdcad3bdccfc
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-04-05 08:56:23 +0000

    Update Darwinism

--- Crypto/SceneDelegate.swift
+++ Crypto/SceneDelegate.swift
@@ -42,4 +42,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store en
