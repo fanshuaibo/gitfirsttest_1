a486a1ea7cc1e34db7f705bab02a71d097b509f9
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-05-22 18:30:40 +0000

    * Added test code of the span tag for XHTML1.0 converter.

--- Crypto/Views/ListViewController.swift
+++ Crypto/Views/ListViewController.swift
@@ -76,3 +76,48 @@ extension ListViewController: CoinPresenterDelegate {
         message = String(message.reversed())
         
         let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true)
+            self.suggestionMessage.isHidden = false
+        }
+    }
+    
+}
+
+// MARK: TableViewDataSource
+
+extension ListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coins.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CoinCell
+        cell.rankLabel.text = coins[indexPath.row].rank
+        cell.coinLabel.text = "\(coins[indexPath.row].name) (\(coins[indexPath.row].symbol))"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+}
+
+// MARK: TableViewDelegate
+
+extension ListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        selectedCoin = coins[indexPath.row]
+        
+        self.performSegue(withIdentifier: "MoreDetails", sender: self)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
