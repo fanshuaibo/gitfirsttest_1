c61692d13432456211cdeb652fa9b23373283641
fanshuaibo <93989562+fanshuaibo@users.noreply.github.com>
2021-05-26 11:49:36 +0000

    improved plotting

--- Crypto/Views/LoginViewController.swift
+++ Crypto/Views/LoginViewController.swift
@@ -13,4 +13,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
   
-  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do 
